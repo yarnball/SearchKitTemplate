5915ef6bc2929683073df971accc03cb061ef4b1
Yarnball <yconomos@gmail.com>
2017-03-15 03:05:30 +1100

    Added <a> element but still rendering all at the same time

---
+++ README.md
@@ -0,0 +1,4 @@
+# SearchKit sample using `create-react-app`
+
+Attempts to add template under /src/Dummy_Site/# SearchKitTemplate
+Still not displaying the list properly
---
+++ src/App.js
@@ -0,0 +1,119 @@
+import React, { Component } from 'react'
+import { extend } from 'lodash'
+import { SearchkitManager,SearchkitProvider,
+  SearchBox, RefinementListFilter, Pagination,
+  HierarchicalMenuFilter, HitsStats, SortingSelector, NoHits,
+  ResetFilters, RangeFilter, NumericRefinementListFilter,
+  ViewSwitcherHits, ViewSwitcherToggle, DynamicRangeFilter,
+  InputFilter, GroupedSelectedFilters,
+  Layout, TopBar, LayoutBody, LayoutResults,
+  ActionBar, ActionBarRow, SideBar } from 'searchkit'
+import './index.css'
+import './custom/responsee.css'
+import './custom/template-style.css'
+
+const host = "http://demo.searchkit.co/api/movies"
+const searchkit = new SearchkitManager(host)
+
+const MovieHitsGridItem = (props)=> {
+  const {bemBlocks, result} = props
+  let url = "http://www.imdb.com/title/" + result._source.imdbId
+  const source:any = extend({}, result._source, result.highlight)
+  return (
+    <div className={bemBlocks.item().mix(bemBlocks.container("item"))} data-qa="hit">
+      <a href={url} target="_blank">
+        <img data-qa="poster" alt="presentation" className={bemBlocks.item("poster")} src={result._source.poster} width="170" height="240"/>
+        <div data-qa="title" className={bemBlocks.item("title")} dangerouslySetInnerHTML={{__html:source.title}}>
+        </div>
+      </a>
+    </div>
+  )
+}
+
+const MovieHitsListItem = (props)=> {
+  const {bemBlocks, result} = props
+  let url = "http://www.imdb.com/title/" + result._source.imdbId
+  const source:any = extend({}, result._source, result.highlight)
+  return (
+    <div className={bemBlocks.item().mix(bemBlocks.container("item"))} data-qa="hit">
+      <div className={bemBlocks.item("poster")}>
+        <img alt="presentation" data-qa="poster" src={result._source.poster}/>
+      </div>
+      <div className={bemBlocks.item("details")}>
+        <a href={url} target="_blank"><h2 className={bemBlocks.item("title")} dangerouslySetInnerHTML={{__html:source.title}}></h2></a>
+        <h3 className={bemBlocks.item("subtitle")}>Released in {source.year}, rated {source.imdbRating}/10</h3>
+        <div className={bemBlocks.item("text")} dangerouslySetInnerHTML={{__html:source.plot}}></div>
+      </div>
+    </div>
+  )
+}
+
+class App extends Component {
+  render() {
+    const Searchbox = SearchBox;
+    const customItemComponent = (props) => {
+
+      const {
+        bemBlocks, onClick, active, disabled, style, itemKey,
+        label, count, showCount, showCheckbox} = props
+      const className = "li222"
+
+      return (
+        <div onClick={onClick}>
+          <div className={className} style={style} data-key={itemKey}>
+            <input type="checkbox" data-qa="checkbox" checked={active} readOnly className="checkbox" ></input>
+            <div className="text">{label}</div>
+            <div className="count">{count}</div>
+          </div>
+        </div>
+      )
+   }
+
+    return (
+      <SearchkitProvider searchkit={searchkit}>
+        <Layout>
+                        <header className="margin-bottom">
+               <div className="line">
+            <nav>
+               <div className="top-nav">
+                  <p className="nav-text"></p>
+                  <a className="logo" href="index.html">            
+                  Movie<span>Blog</span>
+                  </a>            
+                  <h1>SearchKit refinement filters to be on the right </h1>
+                  <ul className="top-ul right">
+                     <li>            
+                            <a><RefinementListFilter id="categories" title="categories" field="type.raw" operator="OR" itemComponent={customItemComponent} /></a>
+                     </li>
+                  </ul>
+               </div>
+            </nav>
+            </div>
+        </header>
+
+        <LayoutBody>
+
+          <SideBar>
+            <RefinementListFilter id="categories" title="Categories" field="type.raw" operator="OR"/>
+          </SideBar>
+          <LayoutResults>
+            <ViewSwitcherHits
+                hitsPerPage={12} highlightFields={["title","plot"]}
+                sourceFilter={["plot", "title", "poster", "imdbId", "imdbRating", "year"]}
+                hitComponents={[
+                  {key:"grid", title:"Grid", itemComponent:MovieHitsGridItem, defaultOption:true},
+                  {key:"list", title:"List", itemComponent:MovieHitsListItem}
+                ]}
+                scrollTo="body"
+            />
+            <NoHits suggestionsField={"title"}/>
+          </LayoutResults>
+
+          </LayoutBody>
+        </Layout>
+      </SearchkitProvider>
+    );
+  }
+}
+
+export default App;
